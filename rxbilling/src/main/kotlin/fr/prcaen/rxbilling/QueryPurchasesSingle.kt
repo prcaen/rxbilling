ea977ece883925c3d4fdf4554ab361089d68b800
Pierrick CAEN <prcaen@gmail.com>
2019-12-03 22:05:49 +0100

    Add queryPurchasesAsync method

---
+++ rxbilling/src/main/kotlin/fr/prcaen/rxbilling/QueryPurchasesSingle.kt
@@ -0,0 +1,55 @@
+@file:JvmName("RxBillingClient")
+@file:JvmMultifileClass
+
+package fr.prcaen.rxbilling
+
+import com.android.billingclient.api.BillingClient
+import com.android.billingclient.api.BillingClient.BillingResponseCode
+import com.android.billingclient.api.BillingClient.SkuType
+import com.android.billingclient.api.Purchase
+import fr.prcaen.rxbilling.exception.RetrievePurchasesException
+import io.reactivex.Scheduler
+import io.reactivex.Single
+import io.reactivex.annotations.CheckReturnValue
+import io.reactivex.annotations.NonNull
+import io.reactivex.annotations.SchedulerSupport
+
+/**
+ * Get purchases details for all the items bought within your app. This method uses a cache of
+ * Google Play Store app without initiating a network request.
+ *
+ * Note: [queryPurchasesAsync] does not operate by default on a particular [Scheduler].
+ *
+ * Error handling: If Billing result is not [BillingResponseCode.OK],
+ * the emitter will deliver [RetrievePurchasesException].
+ *
+ * @property skuType The type of SKU, either "inapp" or "subs" as in [SkuType]
+ *
+ * @see BillingClient.queryPurchases
+ *
+ * @return a Single that emits the list of [Purchase].
+ */
+@CheckReturnValue
+@NonNull
+@SchedulerSupport(SchedulerSupport.NONE)
+fun BillingClient.queryPurchasesAsync(@SkuType skuType: String): Single<List<Purchase>> =
+  Single.create { emitter ->
+    if (emitter.isDisposed) return@create
+
+    val purchasesResult = queryPurchases(skuType)
+
+    if (!emitter.isDisposed) {
+      val result = purchasesResult.billingResult
+
+      if (result.responseCode == BillingResponseCode.OK) {
+        emitter.onSuccess(purchasesResult.purchasesList)
+      } else {
+        emitter.tryOnError(
+          RetrievePurchasesException(
+            responseCode = result.responseCode,
+            debugMessage = result.debugMessage
+          )
+        )
+      }
+    }
+  }
---
+++ rxbilling/src/test/kotlin/fr/prcaen/rxbilling/QueryPurchasesSingleTest.kt
@@ -0,0 +1,97 @@
+package fr.prcaen.rxbilling
+
+import com.android.billingclient.api.BillingClient
+import com.android.billingclient.api.BillingClient.BillingResponseCode
+import com.android.billingclient.api.BillingClient.SkuType.INAPP
+import com.android.billingclient.api.BillingResult
+import com.android.billingclient.api.Purchase
+import com.android.billingclient.api.Purchase.PurchasesResult
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import fr.prcaen.rxbilling.exception.RetrievePurchasesException
+import org.junit.jupiter.api.Test
+import java.util.concurrent.TimeUnit.MILLISECONDS
+
+class QueryPurchasesSingleTest {
+
+  @Test
+  fun `queryPurchasesAsync should return list when BillingResponseCode is OK`() {
+    // Given
+    val skuType = INAPP
+    val client = mock<BillingClient>()
+    val list = listOf(mock<Purchase>())
+    val purchaseResult = mock<PurchasesResult>()
+    val billingResult = mock<BillingResult>()
+
+    doReturn(purchaseResult)
+      .whenever(client)
+      .queryPurchases(skuType)
+
+    doReturn(billingResult)
+      .whenever(purchaseResult)
+      .billingResult
+
+    doReturn(BillingResponseCode.OK)
+      .whenever(billingResult)
+      .responseCode
+
+    doReturn(list)
+      .whenever(purchaseResult)
+      .purchasesList
+
+    // When
+    val obs = client.queryPurchasesAsync(skuType)
+      .test()
+      .apply {
+        awaitTerminalEvent(50, MILLISECONDS)
+      }
+
+    obs.assertValue(list)
+    obs.assertNoErrors()
+  }
+
+  @Test
+  fun `queryPurchasesAsync should throw exception when BillingResponseCode is not OK`() {
+    // Given
+    val skuType = INAPP
+    val client = mock<BillingClient>()
+    val responseCode = BillingResponseCode.ERROR
+    val debugMessage = "Error"
+    val purchaseResult = mock<PurchasesResult>()
+    val billingResult = mock<BillingResult>()
+    val list = listOf(mock<Purchase>())
+
+    doReturn(purchaseResult)
+      .whenever(client)
+      .queryPurchases(skuType)
+
+    doReturn(billingResult)
+      .whenever(purchaseResult)
+      .billingResult
+
+    doReturn(responseCode)
+      .whenever(billingResult)
+      .responseCode
+
+    doReturn(debugMessage)
+      .whenever(billingResult)
+      .debugMessage
+
+    doReturn(list)
+      .whenever(purchaseResult)
+      .purchasesList
+
+    // When
+    val obs = client.queryPurchasesAsync(skuType)
+      .test()
+      .apply {
+        awaitTerminalEvent(50, MILLISECONDS)
+      }
+
+    // Then
+    obs.assertError { e ->
+      e is RetrievePurchasesException && e.responseCode == responseCode && e.debugMessage == debugMessage
+    }
+  }
+}
